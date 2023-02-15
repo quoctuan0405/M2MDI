ddbafd1dbef9f78df5561d859de7b35e5d9ddad1
Your Name <you@example.com>
2023-02-15 10:43:47 +0700

    add seed data

--- DemoRazorPage/Data/ApplicationDBContext.cs
+++ DemoRazorPage/Data/ApplicationDBContext.cs
@@ -5,16 +5,54 @@ namespace DemoRazorPage.Data
 {
     public class ApplicationDBContext : DbContext
     {
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
 
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
         {
 
         }
-        public DbSet<Car> Cars { get; set; }
 
-        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>().HasData(
+                new Car()
+                {
+                    Id = 1,
+                    Name = "Car 1",
+                    Manufacture = "Manufacture 1",
+                    Price = 10000,
+                    Quantity = 10,
+                    ReleaseYear = 2000
+                },
+                new Car()
+                {
+                    Id = 2,
+                    Name = "Car 2",
+                    Manufacture = "Manufacture 2",
+                    Price = 20000,
+                    Quantity = 15,
+                    ReleaseYear = 2002
+                }
+            );
 
-        public DbSet<Cart> Carts { get; set; } 
+            modelBuilder.Entity<User>().HasData(
+                new User()
+                {
+                    Id = 1,
+                    Username = "username",
+                },
+                new User()
+                {
+                    Id = 2,
+                    Username = "username1"
+                }
+            );
 
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ DemoRazorPage/Migrations/20230215033936_add seed.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DemoRazorPage.Migrations
+{
+    /// <inheritdoc />
+    public partial class addseed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Id", "Manufacture", "Name", "Price", "Quantity", "ReleaseYear" },
+                values: new object[,]
+                {
+                    { 1, "Manufacture 1", "Car 1", 10000.0, 10, 2000 },
+                    { 2, "Manufacture 2", "Car 2", 20000.0, 15, 2002 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Username" },
+                values: new object[,]
+                {
+                    { 1, "username" },
+                    { 2, "username1" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
